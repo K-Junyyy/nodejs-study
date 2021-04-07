bcb5bbf9014216dd4755a7c676afcbc5bb6171d2
Cocoon <74912530+K-Junyyy@users.noreply.github.com>
2021-04-07 20:35:38 +0900

    Add files via upload

--- 생활코딩/main.js
+++ 생활코딩/main.js
@@ -3,11 +3,38 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>`;
+}
+
+function templateList(filelist){
+  var list = '<ul>';
+  for (var i = 0; i < filelist.length; i++) {
+    list += `<li><a href="?id=${filelist[i]}">${filelist[i]}</a></li>`;
+  }
+  list += '</ul>';
+  return list;
+}
+
 // http모듈로 서버를 생성한다. 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
+
+  console.log(pathname)
 
   if (pathname === '/') { // pathname이 정상적인 경우
 
@@ -17,61 +44,20 @@ var app = http.createServer(function (request, response) {
 
         var title = 'Welcome';
         var description = "Hello, Node.js";
-        var list = '<ul>';
-
-        for (var i = 0; i < filelist.length; i++) {
-          list += `<li><a href="?id=${filelist[i]}">${filelist[i]}</a></li>`;
-        }
-        list += '</ul>';
-
-        var tamplete = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>`;
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
         response.writeHead(200);
-        response.end(tamplete);
+        response.end(template);
       });
-
-
     }
     else { // CSS, HTML, JavaScript인 경우
-
       fs.readdir('./data', function (error, filelist) {
-
-        var list = '<ul>';
-        for (var i = 0; i < filelist.length; i++) {
-          list += `<li><a href="?id=${filelist[i]}">${filelist[i]}</a></li>`;
-        }
-        list += '</ul>';
-
         fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
           var title = queryData.id;
-          var tamplete = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>`;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);
-          response.end(tamplete);
+          response.end(template);
         });
       });
     }
